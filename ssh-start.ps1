1f3f4df49bb6d1119d4b52192e32e91f838a9a9e
meyner22 <34523035+meyner22@users.noreply.github.com>
2017-12-13 16:20:49 -0500

    Update ssh-start.ps1

--- ssh-start.ps1
+++ ssh-start.ps1
@@ -2,7 +2,7 @@ $ssh = Get-VMHost | Get-VMHostService | where {$_.Key -eq "TSM-SSH"}
 
 foreach ($vmhost in (Get-VMHost)){
 if ($ssh.Running -eq $false){
-		$SSHService | Start-VMHostService -Confirm:$false | select VMHost, Running
+		$ssh | Start-VMHostService -Confirm:$false | select VMHost, Running
 	}
 	else {
 		
